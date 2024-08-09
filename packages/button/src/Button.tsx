fbe5893a8bc492bfa54aa144a3e2e2cdb2071149
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-08-09 08:38:17 +0200

    feat: change button color

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -8,7 +8,7 @@ const Button:FC<Props> = ({children}) => {
         border: "1px solid dimgrey",
         borderRadius: 4,
         backgroundColor: 'black',
-        color: 'white',
+        color: 'red',
         padding: '4px 8px'
     }}>{children}</button>
 }
