fa65b5c9be2de20fe52043f89c5bbd788555999e
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-08-15 06:43:47 +0200

    feat: color is now blue

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -10,7 +10,7 @@ const Button:FC<Props> = ({children}) => {
         border: "1px solid dimgrey",
         borderRadius: 4,
         backgroundColor: 'black',
-        color: 'red',
+        color: 'blue',
         padding: '4px 8px'
     }}>{children}</button>
 }
